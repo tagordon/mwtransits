0bded7a519764fc97541ba2264049174967f10e8
Tyler Gordon <tagordon@uw.edu>
2020-01-31 11:29:23 -0800

    mcmc working

--- mcmc.py
+++ mcmc.py
@@ -1,41 +1,152 @@
 import pymc3 as pm
 import theano.tensor as tt
 from exoplanet.gp import terms, GP
+import numpy as np
+import utils
+import exoplanet as xo
+import matplotlib.pyplot as pl
+%matplotlib inline
 
-with pm.Model() as model:
-    logvar = pm.Normal("logvar", mu=2*np.log(sig), sd=prior_sig)
-
-    # The parameters of the SHOTerm kernel
-    logS0 = pm.Normal("logS0", mu=logs0_init, sd=5.0)
-    logQ = pm.Normal("logQ", mu=logQ_init, sd=5.0)
-    logw0 = pm.Normal("logw0", mu=logw0_init, sd=5.0)
+# make corresponding two-band and one-band datasets 
+def make_data(t, log_S0, log_w0, log_Q, logsig, 
+              t0=5.0, r=0.1, d=1.0, tin=0.1, a=2.0):
     
-    # The parameters for the transit mean function
-    t0 = pm.Normal("t0", mu=t0_init, sd=5.0)
-    BoundedNormal = pm.Bound(pm.Normal, lower=0.0, upper=1.0)
-    r = BoundedNormal("r", mu=r_init, sd=1.0)
-    d = pm.Normal("d", mu=d_init, sd=5.0)
-    tin = pm.Normal("tin", mu=tin_init, sd=5.0)
-            
-    # Deterministics
-    mean = pm.Deterministic("mean", utils.transit(t, t0, r, d, tin))
-
-    # Set up the Gaussian Process model
-    kernel = xo.gp.terms.RotationTerm(
-        log_amp=logamp,
-        period=period,
-        log_Q0=logQ0,
-        log_deltaQ=logdeltaQ,
-        mix=mix
+    transit = utils.theano_transit(t, 
+                                   t0_init, 
+                                   r_init, 
+                                   d_init, 
+                                   tin_init)
+    kernel = xo.gp.terms.SHOTerm(
+        log_S0 = logS0_init,
+        log_w0 = logw0_init,
+        log_Q=logQ_init
     )
     J = 4
+    
+    q = np.array([1, a])
+    Q = q[:, None]*q[None, :]
+    diag = np.exp(2*logsig_init)*np.ones(2, len(t))
+    gp = GP(kernel, t, diag, J=J, Q=Q)
+    n = gp.dot_l(np.random.randn(2*len(t), 1)).eval()
+    transit = tt.reshape(tt.tile(transit, (2, 1)).T, 
+                         (1, 2*transit.shape[0])).T
+    n += transit.eval()
+    return n, np.sum([n[i::2].T[0] for i in range(len(q))], axis=0)
+
+def run_mcmc_2d(t, data, logS0_init, 
+                logw0_init, logQ_init, 
+                logsig_init, t0_init, 
+                r_init, d_init, tin_init, 
+                a_init):
+    
+    with pm.Model() as model:
+        sig = pm.Uniform("sig", lower=-20.0, upper=0.0, testval=logsig_init)
+
+        # The parameters of the SHOTerm kernel
+        logS0 = pm.Uniform("logS0", lower=-20.0, upper=0.0, testval=logS0_init)
+        logQ = pm.Uniform("LogQ", lower=-20.0, upper=20.0, testval=logQ_init)
+        logw0 = pm.Uniform("logw0", lower=-20.0, upper=20.0, testval=logw0_init)
+    
+        a = pm.Uniform("a", lower=1.0, upper=10.0, testval=2.0)
+    
+        # The parameters for the transit mean function
+        t0 = pm.Uniform("t0", lower=t[0], upper=t[-1], testval=t0_init)
+        r = pm.Uniform("r", lower=0.0, upper=1.0, testval=r_init)
+        d = pm.Uniform("d", lower=0.0, upper=10.0, testval=d_init)
+        tin = pm.Uniform("tin", lower=0.0, upper=10.0, testval=tin_init)
             
-    gp = xo.gp.GP(kernel, self.t, self.yerr**2 + tt.exp(logs2), J=J)
+        # Deterministics
+        # mean = pm.Deterministic("mean", utils.transit(t, t0, r, d, tin))
+        transit = utils.theano_transit(t, t0, r, d, tin)
+        transit = tt.reshape(tt.tile(transit, (2, 1)).T, (1, 2*transit.shape[0])).T
 
-    # Compute the Gaussian Process likelihood and add it into the
-    # the PyMC3 model as a "potential"
-    pm.Potential("loglike", gp.log_likelihood([f - mean for f in flux]))
+        # Set up the Gaussian Process model
+        kernel = xo.gp.terms.SHOTerm(
+            log_S0 = logS0,
+            log_w0 = logw0,
+            log_Q=logQ
+        )
+    
+        q = tt.stack(1, a)
+        Q = q[:, None]*q[None, :]
+        diag = np.exp(2*sig)*tt.ones((2, len(t)))
+        gp = GP(kernel, t, diag, J=4, Q=Q)
 
-    # Compute the mean model prediction for plotting purposes
-    #pm.Deterministic("mu", gp.predict())
-    map_soln = xo.optimize(start=model.test_point, verbose=False)
+        # Compute the Gaussian Process likelihood and add it into the
+        # the PyMC3 model as a "potential"
+        pm.Potential("loglike", gp.log_likelihood((data - transit).T))
+
+        # Compute the mean model prediction for plotting purposes
+        #pm.Deterministic("mu", gp.predict())
+        map_soln = xo.optimize(start=model.test_point, verbose=False)
+        
+    with model:
+        map_soln = xo.optimize(start=model.test_point)
+        
+    with model:
+        trace = pm.sample(
+            tune=1000,
+            draws=1000,
+            start=map_soln,
+            cores=2,
+            chains=2,
+            step=xo.get_dense_nuts_step(target_accept=0.9),
+        )
+    return trace
+
+def run_mcmc_1d(t, data, logS0_init, 
+                logw0_init, logQ_init, 
+                logsig_init, t0_init, 
+                r_init, d_init, tin_init):
+    
+    with pm.Model() as model:
+        sig = pm.Uniform("sig", lower=-20.0, upper=0.0, testval=logsig_init)
+
+        # The parameters of the SHOTerm kernel
+        logS0 = pm.Uniform("logS0", lower=-20.0, upper=0.0, testval=logS0_init)
+        logQ = pm.Uniform("LogQ", lower=-20.0, upper=20.0, testval=logQ_init)
+        logw0 = pm.Uniform("logw0", lower=-20.0, upper=20.0, testval=logw0_init)
+        
+        # The parameters for the transit mean function
+        t0 = pm.Uniform("t0", lower=t[0], upper=t[-1], testval=t0_init)
+        r = pm.Uniform("r", lower=0.0, upper=1.0, testval=r_init)
+        d = pm.Uniform("d", lower=0.0, upper=10.0, testval=d_init)
+        tin = pm.Uniform("tin", lower=0.0, upper=10.0, testval=tin_init)
+            
+        # Deterministics
+        # mean = pm.Deterministic("mean", utils.transit(t, t0, r, d, tin))
+        transit = utils.theano_transit(t, t0, r, d, tin)
+
+        # Set up the Gaussian Process model
+        kernel = xo.gp.terms.SHOTerm(
+            log_S0 = logS0,
+            log_w0 = logw0,
+            log_Q=logQ
+        )
+    
+        diag = np.exp(2*sig)*tt.ones((1, len(t)))
+        gp = GP(kernel, t, diag, J=4, Q=Q)
+
+        # Compute the Gaussian Process likelihood and add it into the
+        # the PyMC3 model as a "potential"
+        pm.Potential("loglike", gp.log_likelihood((data - transit).T))
+
+        # Compute the mean model prediction for plotting purposes
+        #pm.Deterministic("mu", gp.predict())
+        map_soln = xo.optimize(start=model.test_point, verbose=False)
+        
+    with model:
+        map_soln = xo.optimize(start=model.test_point)
+        
+    with model:
+        trace = pm.sample(
+            tune=1000,
+            draws=1000,
+            start=map_soln,
+            cores=2,
+            chains=2,
+            step=xo.get_dense_nuts_step(target_accept=0.9),
+        )
+    return trace
+    
+    
